a2d016517e237c4ac3066adac36d97ea5e11075d
uzername <ivanpost777@gmail.com>
2024-07-07 22:55:45 +0300

    more about menu

--- src/scenes/splash.js
+++ src/scenes/splash.js
@@ -20,28 +20,37 @@ export default class splash extends Phaser.Scene {
 
     showScreen() {
         const { width, height } = this.scale
-        this.add.bitmapText(width*0.5, height*0.25, 'modernist', 'Trade Union Dissident: Attorney').setTint(0x9A7D5C)
-            .setOrigin(0.5)
+        this.add.bitmapText(width*0.5, height*0.25, 'modernist', 'Trade Union Dissident: Attorney').setTint(0x9A7D5C).setOrigin(0.5)
         const artDecoBackground = this.add.image(width*0.5, height*0.5,'ui_frame').setDisplaySize(width,height)
         // Play button
-        const playButton = this.add.image(width * 0.5, height * 0.5, 'ui_button3')
-            playButton.setDisplaySize(playButton.width*0.75, playButton.height*0.75)
+        this.playButton = this.add.sprite(width * 0.5, height * 0.5, 'ui_button3').setInteractive();
+        this.playButton.setDisplaySize(this.playButton.width * 0.75, this.playButton.height*0.75)
 
-        this.add.bitmapText(playButton.x, playButton.y, 'modernist', 'Play').setTint(0x9A7D5C).setOrigin(0.5)
+        this.playText = this.add.bitmapText(this.playButton.x, this.playButton.y, 'modernist', 'Play').setTint(0x9A7D5C).setOrigin(0.5)
 
         // Settings button
-        const settingsButton = this.add.image(playButton.x, playButton.y + playButton.displayHeight + 10, 'ui_button2')
-            settingsButton.setDisplaySize(settingsButton.width*0.75, settingsButton.height*0.75)
+        this.settingsButton = this.add.image(this.playButton.x, this.playButton.y + this.playButton.displayHeight + 10, 'ui_button2').setInteractive()
+        this.settingsButton.setDisplaySize(this.settingsButton.width * 0.75, this.settingsButton.height*0.75)
 
-        this.add.bitmapText(settingsButton.x, settingsButton.y, 'modernist', 'Settings').setTint(0x9A7D5C)
+        this.settingsText = this.add.bitmapText(this.settingsButton.x, this.settingsButton.y, 'modernist', 'Settings').setTint(0x9A7D5C)
             .setOrigin(0.5)
 
         // Credits button
-        const creditsButton = this.add.image(settingsButton.x, settingsButton.y + settingsButton.displayHeight + 10, 'ui_button1')
-            creditsButton.setDisplaySize(creditsButton.width * 0.75, creditsButton.height * 0.75)
+        this.creditsButton = this.add.image(this.settingsButton.x, this.settingsButton.y + this.settingsButton.displayHeight + 10, 'ui_button1').setInteractive();
+        this.creditsButton.setDisplaySize(this.creditsButton.width * 0.75, this.creditsButton.height * 0.75)
 
-        this.add.bitmapText(creditsButton.x, creditsButton.y, 'modernist', 'Credits').setTint(0x9A7D5C)
+        this.creditsText = this.add.bitmapText(this.creditsButton.x, this.creditsButton.y, 'modernist', 'Quit').setTint(0x9A7D5C)
             .setOrigin(0.5)
+
+        this.playButton.on('pointerdown', () => {
+            alert('playButton clicked');
+        })
+        this.creditsButton.on('pointerdown', () => {
+            alert('creditsButton \n clicked');
+        })
+        this.settingsButton.on('pointerdown', () => {
+            alert('settingsButton clicked');
+        })
     }
 
 }
---
+++ src/strings.js
@@ -0,0 +1,9 @@
+export default class stringsTraslation {
+    constructor() {
+        this.lines = {
+            'EN': [
+                
+            ]
+        }
+    }
+}
